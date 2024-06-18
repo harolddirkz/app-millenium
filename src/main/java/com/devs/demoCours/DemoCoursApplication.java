5bb1bbc4f42d14ee9a6deda9c46797e1387393bf
rod12151 <rodmel.calderon.27@unsch.edu.pe>
2024-06-18 00:23:03 -0500

    creacion de variables env

--- src/main/java/com/devs/demoCours/DemoCoursApplication.java
+++ src/main/java/com/devs/demoCours/DemoCoursApplication.java
@@ -1,5 +1,6 @@
 package com.devs.demoCours;
 
+import com.devs.demoCours.Configuration.DotenvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoCoursApplication {
 
 	public static void main(String[] args) {
+		DotenvConfig.loadEnv();
 		SpringApplication.run(DemoCoursApplication.class, args);
 	}
 
